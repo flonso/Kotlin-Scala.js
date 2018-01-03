3a90c2a5eca5b190dc419e4baf53d8c993937e2f
Florian Alonso <florian.alonso@epfl.ch>
2018-01-03 23:39:14 +0100

    Fixed tests and moved file to correct folder

--- src/test/scala/BenchmarkTests.scala
+++ src/test/scala/BenchmarkTests.scala
@@ -6,12 +6,26 @@ class BenchmarkTests extends BlackBoxTest {
     val result =
       """
         |LinkedList(0,1,2,3,4,5,6,7,8,9,10)
+        |mapping 0
+        |mapping 1
+        |mapping 2
+        |mapping 3
+        |mapping 4
+        |mapping 5
+        |mapping 6
+        |mapping 7
+        |mapping 8
+        |mapping 9
+        |mapping 10
+        |LinkedList(0,2,4,6,8,10,12,14,16,18,20)
         |LinkedList(1,2,3,4,6,7,9,10)
         |42
         |true false
         |LinkedList(MyListEntry(value=3),MyListEntry(value=95))
         |LinkedList(MyListEntry(value=95))
-        |LinkedList(a,b,c)
+        |a
+        |b
+        |c
       """.stripMargin
     assertExecResult(result,
       Seq(linkedListImpl), mainClass = "benchmarks.collections.LinkedListRunner.main")
@@ -22,7 +36,7 @@ class BenchmarkTests extends BlackBoxTest {
   }
 
   test("Benchmarks - LongMicro") {
-    compileAndCheckIr(Seq("benchmarks/LongMicro.kt", "benchmarks/Benchmark.kt", linkedListImpl), "benchmarks.longmicro.LongMicroAll.main")
+    compileAndCheckIr(Seq("benchmarks/longmicro", "benchmarks/Benchmark.kt", linkedListImpl), "benchmarks.longmicro.LongMicroAll.main")
   }
 
 }
